4a9d2f06dbf9a3619ccfc8fc32d97c58818ed2be
Yuvraj <yuvrajmunjal9@gmail.com>
2025-02-13 13:28:50 -0800

    chore: Add Tailwind CSS and PostCSS configuration files
    ```

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}",
+    "./src/**/*.{html,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
